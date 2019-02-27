6b41a3cdc92d9c1abd0356486064776efda3d567
Zixi Qi <zixiqi@d-172-25-14-212.dhcp.virginia.edu>
2019-02-27 09:59:00 -0500

    2019-2-27

--- array_out_of_order.py
+++ array_out_of_order.py
@@ -13,3 +13,36 @@ For example, a sorted list has zero inversions. The array [2, 4, 1, 3, 5] has th
 (2, 1), (4, 1), and (4, 3). The array [5, 4, 3, 2, 1] has ten inversions: every distinct pair 
 forms an inversion.
 """
+def countAndMerge(nums):
+    if len(nums) <= 1:
+        return nums, 0
+    mid = len(nums) // 2
+    leftNums, leftCount = countAndMerge(nums[:mid])
+    rightNums, rightCount = countAndMerge(nums[mid:])
+    leftIdx, rightIdx = 0, 0
+    count = leftCount + rightCount
+    for i in range(len(nums)):
+        if leftIdx == len(leftNums):
+            nums[i] = rightNums[rightIdx]
+            rightIdx += 1
+        elif rightIdx == len(rightNums):
+            nums[i] = leftNums[leftIdx]
+            leftIdx += 1
+        elif leftNums[leftIdx] <= rightNums[rightIdx]:
+            nums[i] = leftNums[leftIdx]
+            leftIdx += 1
+        else:
+            nums[i] = rightNums[rightIdx]
+            rightIdx += 1
+            count += len(leftNums) - leftIdx
+    return nums, count
+
+
+def countInversion(nums):
+    _, inversions = countAndMerge(nums)
+    return inversions
+
+if __name__ == "__main__":
+    print(countInversion(list(range(10))))
+    print(countInversion(list(reversed(range(10)))))
+    print(countInversion([1,2,3,4,6,5]))
